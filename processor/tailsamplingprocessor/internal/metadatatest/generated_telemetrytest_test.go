5e9592faa3167e10d5166c43c8f089176252c6a8
Chris Marchbanks <csmarchbanks@gmail.com>
2025-12-16 16:38:10 -0700

    [processor/tailsampling] Drop traces exceeding a maximum size
    Allow users to provide a maximum size a trace can reach, if a trace
    exceeds this size before a decision is made then the trace will be
    dropped immediately in order to avoid memory issues or even abuse.

--- processor/tailsamplingprocessor/internal/metadatatest/generated_telemetrytest_test.go
+++ processor/tailsamplingprocessor/internal/metadatatest/generated_telemetrytest_test.go
@@ -33,6 +33,7 @@ func TestSetupTelemetry(t *testing.T) {
 	tb.ProcessorTailSamplingSamplingTraceDroppedTooEarly.Add(context.Background(), 1)
 	tb.ProcessorTailSamplingSamplingTraceRemovalAge.Record(context.Background(), 1)
 	tb.ProcessorTailSamplingSamplingTracesOnMemory.Record(context.Background(), 1)
+	tb.ProcessorTailSamplingTracesDroppedTooLarge.Add(context.Background(), 1)
 	AssertEqualProcessorTailSamplingCountSpansSampled(t, testTel,
 		[]metricdata.DataPoint[int64]{{Value: 1}},
 		metricdatatest.IgnoreTimestamp())
@@ -70,6 +71,9 @@ func TestSetupTelemetry(t *testing.T) {
 		[]metricdata.HistogramDataPoint[int64]{{}}, metricdatatest.IgnoreValue(),
 		metricdatatest.IgnoreTimestamp())
 	AssertEqualProcessorTailSamplingSamplingTracesOnMemory(t, testTel,
+		[]metricdata.DataPoint[int64]{{Value: 1}},
+		metricdatatest.IgnoreTimestamp())
+	AssertEqualProcessorTailSamplingTracesDroppedTooLarge(t, testTel,
 		[]metricdata.DataPoint[int64]{{Value: 1}},
 		metricdatatest.IgnoreTimestamp())
 
